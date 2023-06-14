de758738b44f039dcf5283bdc77dbcf302fff937
Jake Lim <jake.lim@ams-osram.com>
2023-06-14 10:33:20 +0200

    update push.py

--- push.py
+++ push.py
@@ -1,97 +1,133 @@
 import sys
 import dotenv
+import os
 from pathlib import Path
 
 from ppush.manage_git import GitManager
 from ppush.manage_git import GitNotCleanError
 from ppush.manage_remote import RemoteManager
-from ppush.core import CliParser, Manager
-from ppush.core import APP_NAME
+from ppush.core import PushManager, CliParser, Manager, EnvVars
 
-REPO_URL = "git@gittf.ams-osram.info:os-opto-dev/phdf.git"
+from phdf.main import APP_NAME
+
 BASE_DIR = Path(__file__).parent
+REPO_URL = "git@gittf.ams-osram.info:os-opto-dev/phdf.git"
 TEMP_DIRNAME = "Downloads"
 
 
-def run_push():
-    gmr = GitManager(repo_url=REPO_URL, git_base_dir=BASE_DIR)
-    mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-    try:
-        mgr.run_git()
-    except GitNotCleanError as ge:
-        print(repr(ge))
-        sys.exit(1)
-    mgr.run_zip()
-    mgr.cleanup()
-    rmg = RemoteManager()
-    rmg.run(payload_file=mgr.payload_filepath)  # type: ignore
+class PhdfPushManager(metaclass=PushManager):
+    def __init__(self):
+        self.repo_url = REPO_URL
+        self.git_base_dir = BASE_DIR / "phdf"
 
+    def push(self):
+        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
+        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
+        try:
+            mgr.run_git()
+        except GitNotCleanError as ge:
+            print(repr(ge))
+            sys.exit(1)
+        mgr.run_zip()
+        mgr.cleanup()
+        rmg = RemoteManager()
+        rmg.run(payload_file=mgr.payload_filepath)  # type: ignore
 
-def run_push_specify_server(user_input: str):
-    gmr = GitManager(repo_url=REPO_URL, git_base_dir=BASE_DIR)
-    mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-    try:
-        mgr.run_git()
-    except GitNotCleanError as ge:
-        print(repr(ge))
-        sys.exit(1)
-    mgr.run_zip()
-    mgr.cleanup()
-    if "@" in user_input:
-        input_user, input_server = user_input.split("@")
-        rmg = RemoteManager(remote_user=input_user, remote_server=input_server)
-    else:
-        rmg = RemoteManager(remote_server=user_input)
-    rmg.run(payload_file=mgr.payload_filepath)
-
-
-def run_push_simple():
-    # exclude_git_folder
-    gmr = GitManager(repo_url=REPO_URL, git_base_dir=BASE_DIR)
-    mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-    mgr.run_git(skip_fetch=True)
-    mgr.run_zip(exclude_git_folder=True)
-    mgr.cleanup()
-    rmg = RemoteManager()
-    try:
+    def push_specify_server(self, user_input: str):
+        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
+        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
+        try:
+            mgr.run_git()
+        except GitNotCleanError as ge:
+            print(repr(ge))
+            sys.exit(1)
+        mgr.run_zip()
+        mgr.cleanup()
+        if "@" in user_input:
+            input_user, input_server = user_input.split("@")
+            rmg = RemoteManager(remote_user=input_user, remote_server=input_server)
+        else:
+            rmg = RemoteManager(remote_server=user_input)
         rmg.run(payload_file=mgr.payload_filepath)
-    except ConnectionError as e:
-        print(f"ERROR: {e=}")
+
+        gm = GitManager(
+            git_base_dir=self.git_base_dir,
+            repo_url=self.repo_url,
+        )
+        gm.run_ssh_test_command()
+
+    def push_test(self):
+        # exclude_git_folder
+        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
+        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
+        mgr.run_git(skip_fetch=True)
+        mgr.run_zip()
+        mgr.cleanup()
+        rmg = RemoteManager()
+        try:
+            rmg.run(payload_file=mgr.payload_filepath)
+        except ConnectionError as e:
+            print(f"ERROR: {e=}")
+
+    def make_payload(self):
+        """prepares the payload"""
+        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
+        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
+        mgr.run_git()
+        mgr.run_zip()
+        mgr.cleanup()
+
+    def make_payload_simple(self):
+        """makes the payload without ensure all changes are saved and commited"""
+        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
+        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
+        mgr.run_git(skip_fetch=True)
+        mgr.run_zip()
+        mgr.cleanup()
 
 
 def run_test_remote_connx():
-    """test ssh connection to the remote"""
+    """test ssh connection to the remotes"""
     try:
-        print("testing connection to remote server...")
-        rmg = RemoteManager()
-        rmg.connect()
-        print(f" >> ssh connection passed: {rmg.remote_server}")
+        print("============ CONNECTION TESTS ============")
+        print("  -- initiating connection to gittf.ams-osram.info --")
+        gm = GitManager(
+            git_base_dir=BASE_DIR,
+            repo_url=REPO_URL,
+        )
+        gm.run_ssh_test_command()
+
+        print("\n  -- initiating connection to remote tester(s) --")
+        remote_servers = os.environ.get(EnvVars.REMOTE_SERVER_LIST.value)
+        if remote_servers:
+            remote_servers = remote_servers.split(",")
+            n = len(remote_servers)
+            for i, server in enumerate(remote_servers, 1):
+                try:
+                    server = server.strip()
+                    print(
+                        f"[{i}/{n}] testing connection to remote server ({server})..."
+                    )
+                    rmg = RemoteManager(remote_server=server)
+                    rmg.connect()
+                    print(" >> ssh connection successful")
+                except Exception as e:
+                    print(e)
+                    continue
+        else:
+            server = os.environ.get(EnvVars.REMOTE_SERVER.value)
+            print(f"testing connection to single remote server({server}) ...")
+            rmg = RemoteManager()
+            rmg.connect()
+            print(" >> ssh connection successful")
+            print(
+                f"(you can specify a list of servers in \
+{EnvVars.REMOTE_SERVER_LIST.value} to test multiple servers)"
+            )
+        print("========== END OF CONNECTION TESTS ==========")
+
     except Exception as e:
         print(f"connx error. {e=}")
-
-    gm = GitManager(
-        git_base_dir=BASE_DIR,
-        repo_url=REPO_URL,
-    )
-    gm.run_ssh_test_command()
-
-
-def make_payload():
-    """prepares the payload"""
-    gmr = GitManager(repo_url=REPO_URL, git_base_dir=BASE_DIR)
-    mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-    mgr.run_git()
-    mgr.run_zip()
-    mgr.cleanup()
-
-
-def make_payload_simple():
-    """makes the payload without ensure all changes are saved and commited"""
-    gmr = GitManager(repo_url=REPO_URL, git_base_dir=BASE_DIR)
-    mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-    mgr.run_git(skip_fetch=True)
-    mgr.run_zip()
-    mgr.cleanup()
 
 
 def new_dev_function():
@@ -99,53 +135,56 @@ def new_dev_function():
 
 
 def cli():
+    phdf_pm = PhdfPushManager()
     parser = CliParser(
         prog="python push.py",
-        description=f"Tool to push the {APP_NAME} into remote tester(s)",
+        description=f"Tool to push the {APP_NAME.upper()} into remote tester(s)",
         epilog="Usage example: python push.py -p",
     )
+
+    parser.add_argument_and_store_function(
+        "-tc",
+        "--test_connection",
+        action="store_true",
+        help="Test SSH connection to the remote server(s), and gittf",
+        function=run_test_remote_connx,
+    )
+
     parser.add_argument_and_store_function(
         "-p",
         "--push",
         action="store_true",
         help="Runs the full sequence of push and install commands",
-        function=run_push,
+        function=phdf_pm.push,
     )
     parser.add_argument_and_store_function(
-        "-ps",
-        "--push_specify_server",
-        action="store",
-        help="Runs the full sequence of push, manually specify server URL",
-        function=run_push_specify_server,
-    )
-    parser.add_argument_and_store_function(
-        "-pt",
+        "-ppt",
         "--push_test",
         action="store_true",
-        help="Runs a partial sequence of push and install (no fetch, exclude .git)",
-        function=run_push_simple,
+        help="Runs push, but skips git status checks",
+        function=phdf_pm.push,
     )
     parser.add_argument_and_store_function(
-        "-mp",
-        "--make_payload",
+        "-pps",
+        "--phdf_push_specify_server",
+        action="store",
+        help="Runs push, with specified server url",
+        function=phdf_pm.push_specify_server,
+    )
+    parser.add_argument_and_store_function(
+        "-pmp",
+        "--phdf_make_payload",
         action="store_true",
         help="Run git pull, then zip to make the payload",
-        function=make_payload,
+        function=phdf_pm.make_payload,
     )
     parser.add_argument_and_store_function(
-        "-tp",
-        "--test_payload",
+        "-ptp",
+        "--phdf_test_payload",
         action="store_true",
-        help="Same as prepare_payload, but does not check git status. Used \
-            for testing purposes",
-        function=make_payload_simple,
-    )
-    parser.add_argument_and_store_function(
-        "-tc",
-        "--test_connection",
-        action="store_true",
-        help="Test SSH connection to the remote server, and gittf",
-        function=run_test_remote_connx,
+        help="phdf: Same as prepare_payload, but does not check git status. Used \
+for testing purposes",
+        function=phdf_pm.make_payload_simple,
     )
     parser.add_argument_and_store_function(
         "-d",
