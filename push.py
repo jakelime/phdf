51059f3a65f58577ea7a85d45d5abaeff6cc4da2
Jake Lim <jake.lim@ams-osram.com>
2023-06-15 14:52:59 +0200

    fix push.py. working with new ppush api

--- push.py
+++ push.py
@@ -5,14 +5,16 @@ from pathlib import Path
 
 from ppush.manage_git import GitManager
 from ppush.manage_git import GitNotCleanError
-from ppush.manage_remote import RemoteManager
-from ppush.core import PushManager, CliParser, Manager, EnvVars
+from ppush.manage_remote import RemoteInstallManager
+from ppush.core import LocalManager, CliParser
+from ppush.core import PushManager, EnvVars
 
 from phdf.main import APP_NAME
 
 BASE_DIR = Path(__file__).parent
 REPO_URL = "git@gittf.ams-osram.info:os-opto-dev/phdf.git"
-TEMP_DIRNAME = "Downloads"
+LOCAL_TEMP_DIRNAME = "Downloads"
+REMOTE_TEMP_DIRNAME = "Downloads"
 
 
 class PhdfPushManager(metaclass=PushManager):
@@ -20,70 +22,66 @@ class PhdfPushManager(metaclass=PushManager):
         self.repo_url = REPO_URL
         self.git_base_dir = BASE_DIR / "phdf"
 
-    def push(self):
-        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
-        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
+    def push(self, skip_fetch: bool = False):
+        gmr = GitManager(
+            repo_url=self.repo_url,
+            git_base_dir=self.git_base_dir,
+            skip_fetch=skip_fetch,
+        )
+        lmg = LocalManager(git_mgr=gmr, target_tmp_dir=LOCAL_TEMP_DIRNAME)
+        rim = RemoteInstallManager()
+
         try:
-            mgr.run_git()
+            lmg.run()
         except GitNotCleanError as ge:
             print(repr(ge))
             sys.exit(1)
-        mgr.run_zip()
-        mgr.cleanup()
-        rmg = RemoteManager()
-        rmg.run(payload_file=mgr.payload_filepath)  # type: ignore
+        rim.run(
+            payload_file=lmg.payload_filepath,
+            remote_target_dirname=REMOTE_TEMP_DIRNAME,
+            install_to="python_repos",
+        )
 
     def push_specify_server(self, user_input: str):
         gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
-        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
+        lmg = LocalManager(git_mgr=gmr, target_tmp_dir=LOCAL_TEMP_DIRNAME)
+        if "@" in user_input:
+            input_user, input_server = user_input.split("@")
+            rim = RemoteInstallManager(
+                remote_user=input_user, remote_server=input_server
+            )
+        else:
+            rim = RemoteInstallManager(remote_server=user_input)
+
         try:
-            mgr.run_git()
+            lmg.run()
         except GitNotCleanError as ge:
             print(repr(ge))
             sys.exit(1)
-        mgr.run_zip()
-        mgr.cleanup()
-        if "@" in user_input:
-            input_user, input_server = user_input.split("@")
-            rmg = RemoteManager(remote_user=input_user, remote_server=input_server)
-        else:
-            rmg = RemoteManager(remote_server=user_input)
-        rmg.run(payload_file=mgr.payload_filepath)
 
-        gm = GitManager(
-            git_base_dir=self.git_base_dir,
-            repo_url=self.repo_url,
+        rim.run(
+            payload_file=lmg.payload_filepath,
+            remote_target_dirname=REMOTE_TEMP_DIRNAME,
+            install_to="python_repos",
         )
-        gm.run_ssh_test_command()
 
     def push_test(self):
-        # exclude_git_folder
-        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
-        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-        mgr.run_git(skip_fetch=True)
-        mgr.run_zip()
-        mgr.cleanup()
-        rmg = RemoteManager()
-        try:
-            rmg.run(payload_file=mgr.payload_filepath)
-        except ConnectionError as e:
-            print(f"ERROR: {e=}")
+        # skips checking if there are uncommitted changes
+        self.push(skip_fetch=True)
 
-    def make_payload(self):
-        """prepares the payload"""
-        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
-        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-        mgr.run_git()
-        mgr.run_zip()
-        mgr.cleanup()
+    def make_payload(self, skip_fetch: bool = False):
+        """makes the payload file"""
+        gmr = GitManager(
+            repo_url=self.repo_url,
+            git_base_dir=self.git_base_dir,
+            skip_fetch=skip_fetch,
+        )
+        lmg = LocalManager(git_mgr=gmr, target_tmp_dir=LOCAL_TEMP_DIRNAME)
+        lmg.run()
 
     def make_payload_simple(self):
         """makes the payload without ensure all changes are saved and commited"""
-        gmr = GitManager(repo_url=self.repo_url, git_base_dir=self.git_base_dir)
-        mgr = Manager(git_manager=gmr, temp_dirname=TEMP_DIRNAME)
-        mgr.run_git(skip_fetch=True)
-        mgr.run_zip()
-        mgr.cleanup()
+        self.make_payload(skip_fetch=True)
 
 
 def run_test_remote_connx():
@@ -108,8 +106,8 @@ def run_test_remote_connx():
                     print(
                         f"[{i}/{n}] testing connection to remote server ({server})..."
                     )
-                    rmg = RemoteManager(remote_server=server)
-                    rmg.connect()
+                    rmg = RemoteInstallManager(remote_server=server)
+                    rmg.open_ssh()
                     print(" >> ssh connection successful")
                 except Exception as e:
                     print(e)
@@ -117,8 +115,8 @@ def run_test_remote_connx():
         else:
             server = os.environ.get(EnvVars.REMOTE_SERVER.value)
             print(f"testing connection to single remote server({server}) ...")
-            rmg = RemoteManager()
-            rmg.connect()
+            rmg = RemoteInstallManager()
+            rmg.open_ssh()
             print(" >> ssh connection successful")
             print(
                 f"(you can specify a list of servers in \
@@ -158,32 +156,32 @@ def cli():
         function=phdf_pm.push,
     )
     parser.add_argument_and_store_function(
-        "-ppt",
+        "-pt",
         "--push_test",
         action="store_true",
-        help="Runs push, but skips git status checks",
-        function=phdf_pm.push,
+        help="Runs push, but skips git status checks. For testing purposes",
+        function=phdf_pm.push_test,
     )
     parser.add_argument_and_store_function(
-        "-pps",
-        "--phdf_push_specify_server",
+        "-ps",
+        "--push_specify_server",
         action="store",
         help="Runs push, with specified server url",
         function=phdf_pm.push_specify_server,
     )
     parser.add_argument_and_store_function(
-        "-pmp",
-        "--phdf_make_payload",
+        "-pl",
+        "--make_payload",
         action="store_true",
         help="Run git pull, then zip to make the payload",
         function=phdf_pm.make_payload,
     )
     parser.add_argument_and_store_function(
-        "-ptp",
-        "--phdf_test_payload",
+        "-plt",
+        "--make_payload_test",
         action="store_true",
-        help="phdf: Same as prepare_payload, but does not check git status. Used \
-for testing purposes",
+        help="Same as prepare_payload, but does not check git status. \
+For testing purposes",
         function=phdf_pm.make_payload_simple,
     )
     parser.add_argument_and_store_function(
